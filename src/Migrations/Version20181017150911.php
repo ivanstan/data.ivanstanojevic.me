b95644a12bcc79850a629991806548bc027df856
Ivan Stanojevic <ivan@byteout.com>
2018-10-20 21:23:28 +0200

    changed migration

--- src/Migrations/Version20181017150911.php
+++ src/Migrations/Version20181017150911.php
@@ -16,7 +16,6 @@ final class Version20181017150911 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE metar (id INT AUTO_INCREMENT NOT NULL, icao VARCHAR(255) NOT NULL, date DATETIME NOT NULL, metar LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE airports');
     }
 
     public function down(Schema $schema) : void
@@ -24,7 +23,6 @@ final class Version20181017150911 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE airports (id INT DEFAULT NULL, name VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, city VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, country VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, iata VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, icao VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, latitude VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, longitude VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, altitude INT DEFAULT NULL, utf_offset VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, dst VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, timezone VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, type VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci, source VARCHAR(255) DEFAULT NULL COLLATE utf8mb4_general_ci) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('DROP TABLE metar');
     }
 }
