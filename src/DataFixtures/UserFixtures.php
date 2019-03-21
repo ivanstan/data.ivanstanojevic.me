93fee9d08629f4c15343e09accc2a41279e35fc2
Ivan Stanojevic <ivan@byteout.com>
2019-03-21 15:33:40 +0100

    code quality

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -25,23 +25,29 @@ class UserFixtures extends Fixture
     {
         // Add admin user
         $user = new User();
-        $user->setEmail('admin@example.com');
+        $user->setEmail('admin1@example.com');
         $user->setRoles([User::ROLE_ADMIN]);
         $user->setActive(true);
         $user->setVerified(true);
+
         $passwordHash = $this->encoder->encodePassword($user, self::PASSWORD);
         $user->setPassword($passwordHash);
+
         $manager->persist($user);
+
         $this->addReference(self::ADMIN_USER_REFERENCE, $user);
-        for ($i = 1; $i < 10; $i++) {
+
+        for ($i = 2; $i < 11; $i++) {
             $user = new User();
             $user->setEmail("user{$i}@example.com");
             $user->setRoles([User::ROLE_USER]);
             $user->setActive($i > 6);
             $user->setVerified($i < 4 || $i > 8);
             $user->setPassword($passwordHash);
+
             $manager->persist($user);
         }
+
         $manager->flush();
     }
 }
