14bbbd9f18a12439a393f4044084f5f8cbe23d6e
Ivan Stanojevic <ivan@byteout.com>
2019-06-08 22:52:29 +0200

    google analytics twig component

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,18 +16,18 @@ export PORT=2233
 export ARTIFACT_NAME=artifact-`date '+%Y%m%d%H%M%S'`.tar.gz;
 
 print "composer install"
-composer install
-yarn build
+#composer install
+#yarn build
 
 #npm audit
 #bin/phpunit --coverage-text --testsuite Test
 
 tar -czf ${ARTIFACT_NAME} -T ./bin/deploy.list
-scp -r -P ${PORT} ${ARTIFACT_NAME} ${USER}@${HOST}:/${DEPLOY_PATH}/
+scp -rPv ${PORT} ${ARTIFACT_NAME} ${USER}@${HOST}:/${DEPLOY_PATH}/
 rm ${ARTIFACT_NAME}
 
 ssh ${USER}@${HOST} -p${PORT} "cd $DEPLOY_PATH && rm -rf ./assets ./bin ./config ./public/build ./src ./templates ./tests ./translations ./vendor"
 ssh ${USER}@${HOST} -p${PORT} "cd $DEPLOY_PATH && tar -xf $ARTIFACT_NAME && rm $ARTIFACT_NAME"
 
-ssh ${USER}@${HOST} -p${PORT} "$DEPLOY_PATH/bin/console cache:clear --env=prod"
+ssh ${USER}@${HOST} -p${PORT} "$DEPLOY_PATH/bin/console cache:clear"
 ssh ${USER}@${HOST} -p${PORT} "$DEPLOY_PATH/bin/console doctrine:migrations:migrate --no-interaction"
